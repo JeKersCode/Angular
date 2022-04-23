76f26ed02ee42406cbf99b5bb64947ada1245943
JeKersCode <jeka.ignatov1995@gmail.com>
2022-04-23 18:45:30 +0300

    Initial commit

--- mapnote/src/app/app.component.spec.ts
+++ mapnote/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Card'`, () => {
+  it(`should have as title 'pipe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Card');
+    expect(app.title).toEqual('pipe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Card app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pipe app is running!');
   });
 });
